95a8a5c26ca7442342343dcf4fa144ccfd88b6aa
Eduard Sobotnyk <djedj508@gmail.com>
2020-03-09 21:16:47 +0200

    added gantt-schedule-timeline-calendar dependencies and init base code

--- projects/ng-gstc/src/lib/ng-gstc.component.ts
+++ projects/ng-gstc/src/lib/ng-gstc.component.ts
@@ -1,19 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnDestroy, ViewChild, ViewEncapsulation } from '@angular/core';
+import { Config } from 'gantt-schedule-timeline-calendar';
+import GSTC from 'gantt-schedule-timeline-calendar/dist/index.esm';
 
 @Component({
   selector: 'ng-gstc',
-  template: `
-    <p>
-      ng-gstc works!
-    </p>
-  `,
-  styles: []
+  template: `<div class="gstc" #gstc></div>`,
+  styleUrls: ['../../../../node_modules/gantt-schedule-timeline-calendar/dist/style.css'],
+  encapsulation: ViewEncapsulation.None
 })
-export class NgGstcComponent implements OnInit {
+export class NgGstcComponent implements AfterViewInit, OnDestroy {
+  @Input() config: Config;
+  @ViewChild('gstc') calendarEl: ElementRef;
+
+  private gstc: any;
+  private state: any;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit() {
+    const element = this.calendarEl.nativeElement;
+    this.state = GSTC.api.stateFromConfig(this.config);
+    // this.onState.emit(this.state);
+    this.gstc = GSTC({
+      element,
+      state: this.state
+    });
   }
 
+  ngOnDestroy() {
+    this.state.destroy();
+    this.gstc.app.destroy();
+  }
 }
